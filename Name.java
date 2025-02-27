5e25635980cda7b2e0bd028d438eba1410b8d995
sree1888 <sreedurgabandaru@gmail.com>
2025-02-27 22:38:52 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,14 @@
+//Defining class
+class Name
+{
+//Assigning values
+    public static void main(String[] arg){
+        String name = "Sree durga";
+        int rollNo = 20;
+        char section = 'A';
+//Printing the statement        
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+    }
+}
---
+++ javaprogram.java
@@ -0,0 +1,8 @@
+//Defining class
+class javaprogram
+{
+	public static void main(String[] arg){
+//Printing the statement
+	System.out.println("Welcome to java programming");
+}
+}
